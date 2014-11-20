1f482a1e33876507e4bcdc42d54cd05284ab058d
Witoo Harianto <witoo@outlook.com>
2014-11-20 23:05:52 +0700

    update

--- crudmongo/crudmongo.go
+++ crudmongo/crudmongo.go
@@ -1,6 +1,7 @@
 package crudmongo
 
 import (
+	"github.com/plimble/errs"
 	"gopkg.in/mgo.v2"
 )
 
@@ -18,38 +19,29 @@ func (crud *CRUD) Create(v interface{}) error {
 	session := crud.session.Copy()
 	defer session.Close()
 
-	return session.DB(crud.db).C(crud.c).Insert(v)
-}
-
-func (crud *CRUD) IsNotFound(err error) bool {
-	if err != mgo.ErrNotFound {
-		return false
-	}
-
-	return true
-}
-
-func (crud *CRUD) Get(id, v interface{}) error {
-	session := crud.session.Copy()
-	defer session.Close()
-
-	err := session.DB(crud.db).C(crud.c).FindId(id).One(v)
-
-	return err
+	return errs.Mgo(session.DB(crud.db).C(crud.c).Insert(v))
 }
 
 func (crud *CRUD) Delete(id interface{}) error {
 	session := crud.session.Copy()
 	defer session.Close()
 
-	return session.DB(crud.db).C(crud.c).RemoveId(id)
+	return errs.Mgo(session.DB(crud.db).C(crud.c).RemoveId(id))
+}
+
+func (crud *CRUD) Upsert(id, v interface{}) error {
+	session := crud.session.Copy()
+	defer session.Close()
+
+	_, err := session.DB(crud.db).C(crud.c).UpsertId(id, v)
+	return errs.Mgo(err)
 }
 
 func (crud *CRUD) Update(id, v interface{}) error {
 	session := crud.session.Copy()
 	defer session.Close()
 
-	return session.DB(crud.db).C(crud.c).UpdateId(id, v)
+	return errs.Mgo(session.DB(crud.db).C(crud.c).UpdateId(id, v))
 }
 
 func (crud *CRUD) Exist(id interface{}) (bool, error) {
