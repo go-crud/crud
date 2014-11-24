443e0bcf07b7fbcad5eec8e9183daab3e6bac369
wichit ki <wichit@clicknext.co.th>
2014-11-24 18:27:30 +0700

    init sql

---
+++ crudsql/crudsql.go
@@ -0,0 +1,85 @@
+package crudsql
+
+import (
+	"fmt"
+	"github.com/jmoiron/sqlx"
+	"github.com/plimble/errs"
+	"reflect"
+	"strings"
+)
+
+type CRUD struct {
+	db        *sqlx.DB
+	dbName    string
+	tableName string
+}
+
+func New(db *sqlx.DB, dbName, tableName string) *CRUD {
+	return &CRUD{db, dbName, tableName}
+}
+
+func (c *CRUD) Create(v interface{}) error {
+	colNames, vals := readDBTag(v)
+	queryString := fmt.Sprintf("INSERT INTO %s (%s) VALUES (%s)", c.tableName, colNames, stringQuestionMark(len(vals)))
+	_, err := c.db.Exec(queryString, vals...)
+	return err
+}
+
+func (c *CRUD) Delete(id interface{}) error {
+	queryString := fmt.Sprintf("DELETE FROM %s WHERE id=?", c.tableName)
+	result, err := c.db.Exec(queryString, id)
+
+	if i, _ := result.RowsAffected(); i == 0 {
+		return errs.NewNotFound("not found")
+	}
+
+	return err
+
+}
+
+func (c *CRUD) Update(id interface{}, v map[string]interface{}) error {
+	keys, vals := readUpdateKey(v)
+	vals = append(vals, id)
+	queryString := fmt.Sprintf("UPDATE %s SET %s WHERE id=?", c.tableName, keys)
+	result, err := c.db.Exec(queryString, vals...)
+
+	if i, _ := result.RowsAffected(); i == 0 {
+		return errs.NewNotFound("not found")
+	}
+
+	return errs.Sql(err)
+}
+
+func stringQuestionMark(count int) string {
+	return "?" + strings.Repeat(",?", count-1)
+}
+
+func readUpdateKey(v map[string]interface{}) (string, []interface{}) {
+	count := len(v)
+	cols := make([]string, count)
+	vals := make([]interface{}, count)
+	i := 0
+	for key, val := range v {
+		cols[i] = key + "=?"
+		vals[i] = val
+		i++
+	}
+	return strings.Join(cols, ","), vals
+}
+
+func readDBTag(v interface{}) (string, []interface{}) {
+	s := reflect.ValueOf(v).Elem()
+	typeOfT := s.Type()
+	count := s.NumField()
+
+	cols := make([]string, count)
+	vals := make([]interface{}, count)
+
+	for i := 0; i < count; i++ {
+		f := s.Field(i)
+		vals[i] = f.Interface()
+		cols[i] = typeOfT.Field(i).Tag.Get("db")
+	}
+
+	return strings.Join(cols, ","), vals
+}
---
+++ crudsql/crudsql_test.go
@@ -0,0 +1,145 @@
+package crudsql
+
+import (
+	"github.com/jmoiron/sqlx"
+	_ "github.com/mattn/go-sqlite3"
+	"github.com/plimble/errs"
+	"github.com/stretchr/testify/assert"
+	"testing"
+	"time"
+)
+
+type testData struct {
+	ID     int       `db:"id"`
+	Name   string    `db:"name"`
+	Time   time.Time `db:"time"`
+	Height int       `db:"height"`
+	Weight float64   `db:"weight"`
+}
+
+var db *sqlx.DB
+
+func init() {
+	time.Local = time.UTC
+	var err error
+	db, err = sqlx.Connect("sqlite3", ":memory:")
+	if err != nil {
+		panic(err)
+	}
+	schema := `CREATE TABLE IF NOT EXISTS testTable (
+                id int(10) PRIMARY KEY,
+                name varchar(50),
+                time timestamp,
+                height int(10),
+                weight float(10,2)
+            );`
+	if _, err := db.Exec(schema); err != nil {
+		panic(err)
+	}
+}
+
+func getSetup() *CRUD {
+	return New(db, "testDB", "testTable")
+}
+
+func TestReadDBTag(t *testing.T) {
+	now := time.Now().UTC()
+	v := &testData{ID: 11, Name: "Xier", Time: now, Height: 1500, Weight: 25.55}
+	cols, vals := readDBTag(v)
+	assert.Equal(t, "id,name,time,height,weight", cols)
+	assert.Equal(t, []interface{}{11, "Xier", now, 1500, 25.55}, vals)
+}
+
+func TestReadUpdateKey(t *testing.T) {
+	x := map[string]interface{}{
+		"foo": []string{"a", "b"},
+		"bar": "foo",
+		"baz": 10.4,
+	}
+	queryString, vals := readUpdateKey(x)
+
+	assert.Contains(t, queryString, "foo=?")
+	assert.Contains(t, queryString, "bar=?")
+	assert.Contains(t, queryString, "baz=?")
+
+	assert.Contains(t, vals, []string{"a", "b"})
+	assert.Contains(t, vals, "foo")
+	assert.Contains(t, vals, 10.4)
+
+}
+
+func TestUpdate(t *testing.T) {
+	crud := getSetup()
+	now := time.Now()
+
+	v := testData{ID: 13, Name: "Xier", Time: now, Height: 1500, Weight: 25.55}
+	err := crud.Create(&v)
+	assert.NoError(t, err)
+
+	dataForUpdate := map[string]interface{}{
+		"name":   "Wichit",
+		"height": 900,
+		"weight": 44.44,
+	}
+	err = crud.Update(13, dataForUpdate)
+	assert.NoError(t, err)
+
+	v2 := testData{ID: 13, Name: "Wichit", Time: now, Height: 900, Weight: 44.44}
+	var tempData testData
+	err = crud.db.Get(&tempData, "SELECT * FROM testTable WHERE id=13;")
+	assert.NoError(t, err)
+	assert.Equal(t, v2, tempData)
+}
+
+func TestUpdateNotFound(t *testing.T) {
+	crud := getSetup()
+
+	dataForUpdate := map[string]interface{}{
+		"name":   "Wichit",
+		"height": 900,
+		"weight": 44.44,
+	}
+	err := crud.Update(14, dataForUpdate)
+	assert.Error(t, err)
+	assert.True(t, errs.IsNotFound(err))
+}
+
+func TestCreate(t *testing.T) {
+	crud := getSetup()
+	now := time.Now()
+	v := testData{ID: 11, Name: "Xier", Time: now, Height: 1500, Weight: 25.55}
+
+	err := crud.Create(&v)
+	assert.NoError(t, err)
+
+	var tempData testData
+	err = crud.db.Get(&tempData, "SELECT * FROM testTable WHERE id=11;")
+	assert.NoError(t, err)
+	assert.Equal(t, v, tempData)
+}
+
+func TestDelete(t *testing.T) {
+	crud := getSetup()
+	now := time.Now()
+	v := testData{ID: 12, Name: "Xier", Time: now, Height: 1500, Weight: 25.55}
+
+	err := crud.Create(&v)
+	assert.NoError(t, err)
+
+	err = crud.Delete(v.ID)
+	assert.NoError(t, err)
+
+	var tempData testData
+
+	err = crud.db.Get(&tempData, "SELECT * FROM testTable WHERE id=12;")
+	err = errs.Sql(err)
+	assert.Error(t, err)
+	assert.True(t, errs.IsNotFound(err))
+}
+
+func TestDeleteNotFound(t *testing.T) {
+	crud := getSetup()
+	err := crud.Delete("16")
+	assert.Error(t, err)
+	assert.True(t, errs.IsNotFound(err))
+}
