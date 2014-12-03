271da83247e2e5aa62c07d3a4bf54ce654ffc9a7
Witoo Harianto <witoo@outlook.com>
2014-12-03 14:07:54 +0700

    add get

--- crud.go
+++ crud.go
@@ -6,4 +6,5 @@ type CRUD interface {
 	Update(id interface{}, v map[string]interface{}) error
 	Upsert(id, v interface{}) error
 	Exist(id interface{}) (bool, error)
+	GetByID(id interface{}, v interface{}) error
 }
